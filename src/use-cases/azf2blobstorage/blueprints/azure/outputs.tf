28bf8f05b1d9b644212a6b25173164af1c9ad8c1
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2022-07-04 23:33:38 -0300

    Including post and delete methods

--- src/use-cases/azf2blobstorage/blueprints/azure/outputs.tf
+++ src/use-cases/azf2blobstorage/blueprints/azure/outputs.tf
@@ -1,3 +1,9 @@
 output "orama_azure_get_url" {
   value = "https://${azurerm_function_app.funcdeploy-linux.default_hostname}/api/func"
 }
+output "orama_azure_post_url" {
+  value = "https://${azurerm_function_app.funcdeploy-linux.default_hostname}/api/func"
+}
+output "orama_azure_delete_url" {
+  value = "https://${azurerm_function_app.funcdeploy-linux.default_hostname}/api/func"
+}
