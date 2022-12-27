000d0df3f1a8fec9c6f3e2316b2115a91db85714
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2022-12-27 16:27:53 -0300

    Add console.logs

--- src/use-cases/azf2cosmosdb/blueprints/azure/provider.tf
+++ src/use-cases/azf2cosmosdb/blueprints/azure/provider.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+  
   client_id = var.CLIENT_ID
   client_secret = var.CLIENT_SECRET
   subscription_id = var.SUBSCRIPTION_ID
