8faf46172a69b60ca73a53aa5f4663374ca0845c
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2023-01-03 20:52:27 -0300

    Adjusts

--- src/backend/src/config/middlewares.js
+++ src/backend/src/config/middlewares.js
@@ -39,7 +39,11 @@ module.exports = (app) => {
   }))
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(cors(
+  {
+    origin: "*"
+  }
+  ));
   app.use(compression());
   app.use(helmet());
   app.use(limiter);
