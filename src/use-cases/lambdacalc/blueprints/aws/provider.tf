1100975680ff7eaef3e7b3dcd2a95caa3ced562a
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2025-06-30 14:16:01 -0300

    Setting aws provider version

--- src/use-cases/lambdacalc/blueprints/aws/provider.tf
+++ src/use-cases/lambdacalc/blueprints/aws/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
 provider "aws" {
     region  = var.region
     access_key = var.AWS_ACCESS_KEY_ID
