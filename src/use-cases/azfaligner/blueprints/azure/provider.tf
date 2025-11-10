903fa1224de6c4dd605d30b7cc02bd639adb590e
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2025-11-10 17:30:48 -0300

    Add AWS Benchmark for aligner in Seeds

--- src/use-cases/azfaligner/blueprints/azure/provider.tf
+++ src/use-cases/azfaligner/blueprints/azure/provider.tf
@@ -1,11 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.99.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
-  }  
-  client_id = var.CLIENT_ID
-  client_secret = var.CLIENT_SECRET
+  }
+
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
   subscription_id = var.SUBSCRIPTION_ID
-  tenant_id = var.TENANT_ID
+  tenant_id       = var.TENANT_ID
 }
